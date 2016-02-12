305bbf0c5b725ef92f928979f78c18f1ee1643fd
Johannes Raggam <thetetet@gmail.com>
2016-02-12 14:17:41 +0100

    add traverser to view

--- src/collective/proxytype/proxyview.py
+++ src/collective/proxytype/proxyview.py
@@ -1,11 +1,14 @@
 from .proxy import get_content
-from Products.Five import BrowserView
+from Products.Five.browser import BrowserView
+from zope.interface import implementer
+from zope.publisher.interfaces import IPublishTraverse
 
 
 # TODO: traverser for urls pointing into context's url space
 # TODO: caching
 
 
+@implementer(IPublishTraverse)
 class ProxyView(BrowserView):
 
     def get_content(self):
@@ -14,3 +17,14 @@ class ProxyView(BrowserView):
             self.context.absolute_url(),
             self.context.url_replacement_map
         )
+
+    # Traverser
+
+    def __call__(self):
+        return self.subpath
+
+    def publishTraverse(self, request, name):
+        if not hasattr(self, 'subpath'):
+            self.subpath = []
+        self.subpath.append(name)
+        return self
