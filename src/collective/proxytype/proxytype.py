a916dc6ffabbc3a951a9d6f5a88c90049562b6ac
Johannes Raggam <thetetet@gmail.com>
2017-02-16 11:44:50 +0100

    actually no need for IProxyTypeSchema

--- src/collective/proxytype/proxytype.py
+++ src/collective/proxytype/proxytype.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
-from .interfaces import IProxySchema
 from .interfaces import IProxyType
 from plone.dexterity.content import Container
 from zope.interface import implementer
-from plone.supermodel import model
-
-
-class IProxyTypeSchema(model.Schema, IProxySchema):
-    """Proxy content schema
-    """
 
 
 @implementer(IProxyType)
